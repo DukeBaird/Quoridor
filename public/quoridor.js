1bd477da6fbbe2009a2d5001b2b91e30cf834434
Ben Craig <benjamin.d.craig@gmail.com>
2014-07-19 13:32:27 -0400

    added canvas and client js file

---
+++ public/quoridor.js
@@ -0,0 +1,17 @@
+var canvas = $("#canvas")[0];
+var ctx = canvas.getContext("2d");
+var w = $("#canvas").width();
+var h = $("#canvas").height();
+var cellwidth = 50;
+
+function draw_board(){
+
+}
+
+
+function start_game(){
+	draw_board();
+}
+
+
+start_game();
