63f70360883de47c9c744e666303924694984550
Ben Craig <Benjamin.d.craig@gmail.com>
2014-07-21 12:41:06 -0400

    Draws Board

--- public/quoridor.js
+++ public/quoridor.js
@@ -5,7 +5,14 @@ var h = $("#canvas").height();
 var cellwidth = 50;
 
 function draw_board(){
-
+    for (var x =0; x < 9; x++){
+        for (var y = 0; y < 9; y++){
+            ctx.fillStyle = "brown";
+            ctx.fillRect(50 + x*100, 50 + y*100, 100, 100);
+            ctx.strokeStyle = "white";
+	        ctx.strokeRect(50 + x*100, 50 + y*100, 100, 100);
+        }
+    }
 }
 
 
