81b67582fbffc9b5bd3a476a0744072f8db8b11e
Ben Craig <benjamin.d.craig@gmail.com>
2014-08-06 15:58:02 -0400

    Draw barriers properly

--- public/quoridor.js
+++ public/quoridor.js
@@ -7,10 +7,10 @@ var turn_player = 1;
 var p1_barriers = 10;
 var p2_barriers = 10;
 var board = [
-  "EXEXEXEX1XEXEXEXE",
-  "XXXXXXXXXXXXXXXXX",
-  "EXEXEXEXEXEXEXEXE",
-  "XXXXXXXXXXXXXXXXX",
+  "EBEXEXEX1XEXEXEXE",
+  "XBBXXXXXXXXXXXXXX",
+  "EBEXEXEXEXEXEXEXE",
+  "XBBBXXXXXXXXXXXXX",
   "EXEXEXEXEXEXEXEXE",
   "XXXXXXXXXXXXXXXXX",
   "EXEXEXEXEXEXEXEXE",
@@ -38,12 +38,43 @@ function draw_board(){
     for (var y=0; y < this.height; y++){
         for (var x=0; x < this.width; x++){
             if (board[y].charAt(x) === "E"){
+              ctx.fillStyle = "brown";
+              ctx.fillRect(50 + x*50, 50 + y*50,100,100);
+              ctx.strokeStyle = "white";
+	            ctx.strokeRect(50 + x*50, 50 + y*50, 100, 100)
+            } else if (board[y].charAt(x) === "1"){
+              ///draw p1
                 ctx.fillStyle = "brown";
                 ctx.fillRect(50 + x*50, 50 + y*50,100,100);
                 ctx.strokeStyle = "white";
-	            ctx.strokeRect(50 + x*50, 50 + y*50, 100, 100)
+                ctx.strokeRect(50 + x*50, 50 + y*50, 100, 100)
+                ctx.fillStyle = "green";
+                ctx.beginPath();
+                ctx.arc(100 + x*50, 100 + y*50, 45, 0, 2* Math.PI);
+                ctx.fill();
+
+            } else if (board[y].charAt(x) === "2"){
+              //draw p2
+              ctx.fillStyle = "brown";
+              ctx.fillRect(50 + x*50, 50 + y*50,100,100);
+              ctx.strokeStyle = "white";
+              ctx.strokeRect(50 + x*50, 50 + y*50, 100, 100)
+              ctx.fillStyle = "blue";
+              ctx.beginPath();
+              ctx.arc(100 + x*50, 100 + y*50, 45, 0, 2* Math.PI);
+              ctx.fill();
             }
         }
+    }
+
+    for (var y=0; y < this.height; y++){
+      for (var x=0; x < this.width; x++){
+        if (board[y].charAt(x) === "B"){
+            //Draw Barrier
+            ctx.fillStyle = "black";
+            ctx.fillRect(95 + 50*x, 95 + 50*y, 10,10);
+          }
+      }
     }
 }
 
