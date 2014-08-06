e1efdd9f28a768ec3afa199d4a2bc187228b0f1d
Ben Craig <Benjamin.d.craig@gmail.com>
2014-08-06 15:26:29 -0400

    Update quoridor.js

--- public/quoridor.js
+++ public/quoridor.js
@@ -2,44 +2,51 @@ var canvas = $("#canvas")[0];
 var ctx = canvas.getContext("2d");
 var w = $("#canvas").width();
 var h = $("#canvas").height();
-// var bard = 2d array?
 var cellwidth = 50;
 var turn_player = 1;
-var player_1 = [4,0];
-var player_2 = [4,8];
 var p1_barriers = 10;
 var p2_barriers = 10;
+var board = [
+  "EXEXEXEX1XEXEXEXE",
+  "XXXXXXXXXXXXXXXXX",
+  "EXEXEXEXEXEXEXEXE",
+  "XXXXXXXXXXXXXXXXX",
+  "EXEXEXEXEXEXEXEXE",
+  "XXXXXXXXXXXXXXXXX",
+  "EXEXEXEXEXEXEXEXE",
+  "XXXXXXXXXXXXXXXXX",
+  "EXEXEXEXEXEXEXEXE",
+  "XXXXXXXXXXXXXXXXX",
+  "EXEXEXEXEXEXEXEXE",
+  "XXXXXXXXXXXXXXXXX",
+  "EXEXEXEXEXEXEXEXE",
+  "XXXXXXXXXXXXXXXXX",
+  "EXEXEXEXEXEXEXEXE",
+  "XXXXXXXXXXXXXXXXX",
+  "EXEXEXEX2XEXEXEXE"
+  ];
+
+
 
 function draw_board(){
+    this.height = board.length;
+    this.width = board[0].length;
+    
     ctx.fillStyle = "white";
     ctx.fillRect(0,0,1000,1000);
-    
-    for (var x = 0; x < 9; x++){
-        for (var y = 0; y < 9; y++){
-            ctx.fillStyle = "brown";
-            ctx.fillRect(50 + x*100, 50 + y*100, 100, 100);
-            ctx.strokeStyle = "white";
-	        ctx.strokeRect(50 + x*100, 50 + y*100, 100, 100);
+   
+    for (var y=0; y < this.height; y++){
+        for (var x=0; x < this.width; x++){
+            if (board[y].charAt(x) === "E"){
+                ctx.fillStyle = "brown";
+                ctx.fillRect(50 + x*50, 50 + y*50,100,100);
+                ctx.strokeStyle = "white";
+	            ctx.strokeRect(50 + x*50, 50 + y*50, 100, 100)
+            }
         }
     }
 }
 
-function draw_players(){
-    ctx.fillStyle="green";
-    ctx.strokeStyle = "green";
-    ctx.beginPath();
-    ctx.arc(100 + player_1[0]*100,100 + player_1[1]*100,45,0,2*Math.PI);
-    ctx.stroke();   
-    ctx.fill();
-    
-    ctx.fillStyle="blue";
-    ctx.strokeStyle = "blue";
-    ctx.beginPath();
-    ctx.arc(100 + player_2[0]*100,100 + player_2[1]*100,45,0,2*Math.PI);
-    ctx.stroke();   
-    ctx.fill();
-    
-}
 
 function draw_Barriers(){
     
@@ -55,22 +62,9 @@ function draw_Barriers(){
 
 function start_game(){
 	draw_board();
-    draw_players();
     draw_Barriers();
 }
 
-function swap_turn(){
-     console.log("change turn player");   
-}
 
-$("#move").on("click",function(){
-   console.log("moving!");
-    swap_turn();
-});
-
-$("#barrier").on("click",function(){
-    console.log("placing barrier!");   
-    swap_turn();
-});
     
 start_game();
